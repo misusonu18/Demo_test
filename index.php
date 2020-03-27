fc33ae6729e77ad24429d732819699e9d7f9e6db
misusonu18@gmail.com <52093984+misusonu18@users.noreply.github.com>
2020-03-27 17:31:08 +0530

    5 Random number

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-    // date_default_timezone_set("Asia/Kolkata");
-    // $date = date("d/m/Y"." "."h:i:sa");
-    // echo $date;
-
     for ($i = 0; $i < 5; $i++) {
         echo " ".rand(10,100);
     }  
